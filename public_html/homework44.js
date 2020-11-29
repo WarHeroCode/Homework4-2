31cd2bd3d88833d19420c4c5b978ec17d99641cc
Simon Molina <72926822+WarHeroCode@users.noreply.github.com>
2020-11-28 23:36:33 -0600

    commiting

---
+++ public_html/homework44.js
@@ -0,0 +1,21 @@
+let numbers = [];
+while(numbers.length <=19){
+    numbers.push(Number(prompt("Enter number ")));
+    numbers.sort((a, b) => a - b);
+}
+
+document.write("Numbers- " + numbers );
+
+let smallest = numbers[0];
+document.write("<br> Smallest- " + smallest);
+
+let biggest = numbers[19];
+document.write("<br> Biggest- " + biggest);
+
+let total = 0;
+    total += numbers;
+    
+document.write("<br> Total sum- " + total);
+
+let average= total/20;
+document.write("<br> Average- " + average);
